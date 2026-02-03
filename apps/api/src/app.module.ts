dceba13c49162a32a9d884620c79b6fc9bbbcf05
DanielJohn17 <danielyohannes38@gmail.com>
2026-02-03 13:07:59 +0300

    env

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -17,7 +17,7 @@ import { NotificationsModule } from './notifications/notifications.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['.env', '../../.env'],
+      envFilePath: [ '../../.env', '.env'],
     }),
     DatabaseModule,
     VehicleModule,
